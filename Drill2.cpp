e0106ff7a0c9503e4682282c86068faa6060a9ab
uzsibox <90861681+uzsibox@users.noreply.github.com>
2021-09-24 07:15:48 +0200

    Initial commit

---
+++ Drill2.cpp
@@ -0,0 +1,39 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout << "Enter the name of the person you want to write to (followed by 'enter'):\n";
+	string first_name;
+	cin >> first_name;
+	cout << "Dear, " << first_name << " How are you? I am fine. I miss you!\n";
+	cout << "Enter the name of your friend (followed by 'enter'):\n";
+	string friend_name;
+	cin >> friend_name;
+	cout << "Have you seen " << friend_name << " lately ? \n";
+	char friend_sex = '0';
+	cout << "What sex is " << friend_name << "  ?(m/f) \n";
+	cin >> friend_sex;
+	if (friend_sex == 'm') {
+		cout << "If you see " << friend_name << " please ask him to call me.\n";
+	}
+	if (friend_sex == 'f') {
+		cout << "If you see " << friend_name << " please ask her to call me.\n";
+	
+	}
+	cout << "Enter the age of the recipient(followed by 'enter'):\n"; 
+	int age = 0;
+	cin >> age;
+	cout << "I hear you just had a birthday and you are " << age << " years old.\n";
+	if (age <= 0 || age >= 110) { simple_error("you're kidding!");
+	}
+	if (age < 12) {
+		cout << "Next year you will be " << age + 1 << " . \n";
+	}
+	if (age == 17) {
+		cout << "Next year you will be able to vote. \n";
+	}
+	if (age > 70) {
+		cout << "I hope you are enjoying retirement. \n";
+	}
+	cout << "Yours sincerely \n\n Attila Balogh";
+}
